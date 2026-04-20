cf4c9d96732d4002c4ffc58b4854ed431154858d
Brandon <137568790+Brandondiaz19@users.noreply.github.com>
2026-04-19 22:41:15 -0400

    Entrono Virtual creado

---
+++ app-streaming/electron/main.js
@@ -0,0 +1,32 @@
+import { app, BrowserWindow, Menu, process } from 'electron'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    title: 'app-streaming',
+    autoHideMenuBar: true,
+  })
+
+  win.loadURL('http://localhost:5173')
+
+  Menu.setApplicationMenu(null)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
